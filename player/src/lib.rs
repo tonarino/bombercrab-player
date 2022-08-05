f993c24dc84238c81880ab8d99d373395f3f7423
Pablo Mansanet <pablomansanet@gmail.com>
2022-08-05 10:47:26 +0900

    Misc fixes and wording tweaks

--- player/src/lib.rs
+++ player/src/lib.rs
@@ -5,23 +5,24 @@ use bomber_lib::{
 };
 use bomber_macro::wasm_export;
 
+/// Player struct. Can contain any arbitrary data, which will carry over between turns.
 #[derive(Default)]
-struct Fool;
+struct MyPlayer;
 
 #[wasm_export]
-impl Player for Fool {
+impl Player for MyPlayer {
     fn act(
         &mut self,
         _surroundings: Vec<(Tile, Option<Object>, Option<Enemy>, bomber_lib::world::TileOffset)>,
     ) -> Action {
-        todo!("Act on surroundings and return an Action to do")
+        todo!("Observe your surroundings and return an Action to do this turn")
     }
 
     fn name(&self) -> String {
-        todo!("Return your name, mind character limit")
+        todo!("Return your name within the 10 character limit")
     }
 
     fn team_name() -> String {
-        todo!("Return your team name, mind character limit")
+        todo!("Return your team name within the 20 character limit")
     }
 }
